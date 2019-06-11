ed6a612cf62bc131d6226c37ace17d98d999194f
loctx-0896 <tran.xuan.loc@sun-asterisk.com>
2019-06-11 13:16:21 +0700

    chapter_5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,25 @@
-# frozen_string_literal: true
-
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
-  test "should get testing" do
-    get static_pages_testing_url
+  test "should get contact" do
+    get contact_url
+    puts contact_url
+    puts contact_path
     assert_response :success
   end
 end

69e51e106786ca377abfc7ecc0eda99769e14214
loctx-0896 <tran.xuan.loc@sun-asterisk.com>
2019-06-13 16:55:19 +0700

    chapter_8

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
   validates :password, presence: true,
             length: {minimum: Settings.users.password.minimum}
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
 end

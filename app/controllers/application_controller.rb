8afd27fa3f8586e254b389d6cb066e1521ecc61a
loctx-2273 <tran.xuan.loc@sun-asterisk.com>
2020-09-12 17:35:08 +0700

    pull_38

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    # fsd
   end
 
   def default_url_options
